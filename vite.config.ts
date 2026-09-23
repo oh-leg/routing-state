2745538a6f72d896bae2c1ade680df16b336b124
oh-leg <art.ohleg@yandex.ru>
2026-09-23 22:45:58 +0400

    fix gh 404?

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import { vitePages } from '@kingironman2011/vite-pages'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), vitePages()],
   base: '/routing-state/',
 })
