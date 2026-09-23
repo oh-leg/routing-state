7a6f63a1d09073d9aa0f9c27af04f567082e30e1
oh-leg <art.ohleg@yandex.ru>
2026-09-23 22:34:45 +0400

    release

--- src/main.tsx
+++ src/main.tsx
@@ -9,7 +9,7 @@ import './styles/global.css';
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <BrowserRouter basename="/routing-state">
         <App />
       </BrowserRouter>
     </Provider>
